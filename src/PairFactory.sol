379a5dceec34dbdf226a5d64fa746a2f63be4232
Priapisman677 <expeliamm02@hotmail.com>
2025-06-07 07:43:01 -0700

    u

--- src/PairFactory.sol
+++ src/PairFactory.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.30;
 
 import {Pair} from "../src/Pair.sol";
 
+interface IToken {
+    function name() external view returns (string memory);
+}
+
+
 contract PairFactory {
 
     address[] public allPairs;
@@ -11,14 +16,14 @@ contract PairFactory {
         return allPairs;
     }
 
-    event PairCreated(address indexed pair, address token0, address token1);
+    event PairCreated(address indexed pair, address token0, address token1, string token0Name, string token1Name);
 
     function createPair(address token0, address token1) public returns(address){
         Pair pair = new Pair(token0, token1);
 
         allPairs.push(address(pair));
 
-        emit PairCreated(address(pair)  ,token0, token1); //✨✨ After reviewing, I believe it doesn't matter the order the tokens are passed✨✨
+        emit PairCreated(address(pair)  ,token0, token1, IToken(token0).name(), IToken(token1).name()); //✨✨ After reviewing, I believe it doesn't matter the order the tokens are passed✨✨
 
         return address(pair);
     }
